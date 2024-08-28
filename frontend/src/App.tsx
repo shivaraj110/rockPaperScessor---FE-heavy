2d0fbef20c36311542ecc97fcd84875e8c44fe32
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-08-28 14:18:29 +0530

    randomizing computer choice

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { ChoiceCard } from './components/ChoiceCard'
+import { RecoilRoot } from 'recoil'
+import { Result } from './components/Result'
 
 function App() {
   return (
     <>
     <div className="bg-slate-300 h-screen flex justify-center items-center">
-    <ChoiceCard/>
+      <RecoilRoot><ChoiceCard/>
+      <Result/></RecoilRoot>
     </div>
     </>
   )
